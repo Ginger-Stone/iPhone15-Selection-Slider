e9832c8d029d80188e23e22fd56dc72722cc37db
Doreen Chemweno <doreenjeptoo@gmail.com>
2023-07-25 05:18:17 +0300

    added animation

--- script.js
+++ script.js
@@ -9,15 +9,31 @@ function focusOn(slide) {
 
   newFocus.classList.add("focus");
   newFocus.classList.remove("unfocus");
-  element.classList.contains("unfocusTop")
-    ? element.classList.remove("unfocusTop")
-    : element.classList.remove("unfocusBottom");
+  newFocus.classList.contains("unfocusTop")
+    ? newFocus.classList.remove("unfocusTop")
+    : newFocus.classList.remove("unfocusBottom");
 
-  slides.forEach((element) => {
+  let focusNo = Number(/[0-9]/.exec(slide)[0]);
+  console.log(focusNo);
+  let unfocusBottomNo = focusNo + 1 > 2 ? 0 : focusNo + 1;
+  let unfocusTopNo = unfocusBottomNo + 1 > 2 ? 0 : unfocusBottomNo + 1;
+  console.log(focusNo, unfocusBottomNo, unfocusTopNo);
+
+  [...slides].forEach((element, index) => {
     if (element.id !== slide) {
       if (element.classList.contains("focus"))
         element.classList.remove("focus");
       element.classList.add("unfocus");
+      if (index == unfocusBottomNo) {
+        element.classList.add("unfocusBottom");
+        if (element.classList.contains("unfocusTop"))
+          element.classList.remove("unfocusTop");
+      }
+      if (index == unfocusTopNo) {
+        if (element.classList.contains("unfocusBottom"))
+          element.classList.remove("unfocusBottom");
+        element.classList.add("unfocusTop");
+      }
     }
   });
 }
